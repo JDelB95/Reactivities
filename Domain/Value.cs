e6b58c4926f52e67edc1cee8ed45b1af46eb2aeb
John Del Bianco <John Del Bianco>
2020-07-31 14:26:57 -0400

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        //If a property inside one of our entities is given the name Id it will automatically be the primary key.
+        public int Id {get; set;}//Auto implemented properties. No additional logic
+        public string Name {get; set;}
+    }
+}
---
+++ Persistence/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        { 
+        }
+        public DbSet<Value> Values {get; set;} //Values is the table name in SQLite
+        //Protected means it's only accessible in the class it's defined in and any derived classes from this class
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>()
+            .HasData(
+                new Value {Id = 1, Name = "Value 101"},
+                new Value {Id = 2, Name = "Value 102"},
+                new Value {Id = 3, Name = "Value 103"}
+            );
+        }
+    }
+}
